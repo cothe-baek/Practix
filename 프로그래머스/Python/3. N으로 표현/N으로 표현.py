678a42c68da41213a369dca532ae3932bd9d3fdd
cothe-baek <gyuul173@gmail.com>
2026-08-08 22:46:24 +0900

    Update N으로 표현.py

--- 프로그래머스/Python/3. N으로 표현/N으로 표현.py
+++ 프로그래머스/Python/3. N으로 표현/N으로 표현.py
@@ -14,27 +14,29 @@ dp[i]: N을 i번 사용해 만들 수 있는 수들의 집합
 그렇게 안하면 완성되지 않은 dp 칸을 사용해 다음 dp값을 계산하는 불상사가 발생함
 
 pull로 할거면 dp[i]: = dp[j] + dp[i-j] 로 하면 되는데, 이건 불상사 생각 안해도 되는 듯
-
+대신 이때 종료조건 위치랑 어차피 1,3 / 2,2 / 3,1 처럼 바뀐 상황을 자동으로 계산하니
+아래 push처럼 n, m 자리바꿈 계산은 안해도 됨
 """
 
 def solution(N, number):
     answer = 10
-    nlst = [0, N, 10*N+N, 100*N+10*N+N, 1000*N+100*N+10*N+N, 10000*N+1000*N+100*N+10*N+N]
     
     dp = [set() for _ in range(9)]
     # dp[0].add(0)
     
     for i in range(1, 5):
-        dp[i].add(nlst[i])
-    
+        dp[i].add(int(str(N)*i))
     
     for i in range(1, 9):
+        if number in dp[i]:
+            return i
+        
         for j in range(1, i+1):
             if i + j > 8:
                 continue
-            
-            for n in dp[i]:
-                for m in dp[j]:
+                
+            for n in dp[j]:
+                for m in dp[i]:
                     if n - m > 0:
                         dp[i+j].add(n - m)
                     elif m - n > 0:
@@ -43,14 +45,7 @@ def solution(N, number):
                     dp[i+j].add(n * m)
                     if m > 0:
                         dp[i+j].add(n / m)
-                    elif n > 0:
-                        dp[i+k].add(m / n)
+                    if n > 0:
+                        dp[i+j].add(m / n)
             
-    for i in range(1, 9):
-        if number in dp[i]:
-            answer = min(i, answer)
-    
-    if answer == 10:
-        return -1
-    else:
-        return answer
+    return -1
