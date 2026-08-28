9e2854e7cf4da9c6467bb524550351e3b195c656
cothe-baek <gyuul173@gmail.com>
2026-08-28 15:50:22 +0900

    [level 3] Title: 정수 삼각형, Time: 74.31 ms, Memory: 22.7 MB -BaekjoonHub

--- 프로그래머스/Python/3. 정수 삼각형/README.md
+++ 프로그래머스/Python/3. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
 
 ### 성능 요약
 
-메모리: 22.4 MB, 시간: 50.33 ms
+메모리: 22.7 MB, 시간: 74.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 09일 13:59:38
+2026년 08월 28일 15:50:19
 
 ### 문제 설명
 
--- 프로그래머스/Python/3. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/Python/3. 정수 삼각형/정수 삼각형.py
@@ -1,33 +1,32 @@
-"""
-dp[i][j]: i, j까지 왔을 때 거쳐온 숫자의 합 최댓값
-"""
-
-def solution(triangle):
-    answer = 0
-    N = len(triangle)
+def myp(arr):
+    for row in arr:
+        for val in row:
+            print(val, end=' ')
+        print()
+    print()
     
+def solution(tri):
+    N = len(tri)
     arr = [[0]*N for _ in range(N)]
-    dp = [[0]*(N) for _ in range(N)]
     
     for i in range(N):
         for j in range(i+1):
-            arr[i][j] = triangle[i][j]
+            arr[i][j] = tri[i][j]
+    # myp(arr)
     
+    """
+    dp[i][j]: 거쳐간 숫자의 합의 최댓값
+    
+    이런 문제는 일단 0,0을 두면 좋은가 나쁜가
+    """
+    dp = [[-1]*N for _ in range(N)]
     dp[0][0] = arr[0][0]
+    
     for i in range(N-1):
-        for j in range(i+1):
-            # 아래로 이동
+        for j in range(N-1):
+            # 아래로
             dp[i+1][j] = max(dp[i+1][j], dp[i][j] + arr[i+1][j])
-            # 오른쪽 아래로 이동
+            # 오른쪽 아래로
             dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j] + arr[i+1][j+1])
-            
-    answer = max(dp[-1])
-        
     
-    # for row in dp:
-    #     for val in row:
-    #         print(f"{val:>2}", end=" ")
-    #     print()
-    # print()
-    
-    return answer
+    return max(dp[N-1])
