ada0a142dc95fc42776d276f39eb7027d975ed40
cothe-baek <gyuul173@gmail.com>
2026-08-28 15:27:47 +0900

    [level 3] Title: N으로 표현, Time: 6.15 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/Python/3. N으로 표현/N으로 표현.py
+++ 프로그래머스/Python/3. N으로 표현/N으로 표현.py
@@ -1,51 +1,43 @@
-"""
-dp[i]: N을 i번 사용해서 number를 만들 수 있는지
-dp[i][j]: N을 i번 사용해 j를 만들 수 있는지
-얘넨 탈락
-
-dp[i]: N을 사용해 i를 만들 수 있는 최소 개수
-당첨이 아니네용 괄호 연산을 위해서는
-
-dp[i]: N을 i번 사용해 만들 수 있는 수들의 집합
-을 사용해야 한다고 하네요
-
-이러면 dp[i+j]: dp[i] 와 dp[j]의 연산으로 나올 수 있는 수들의 집합
-주의할 건, 이렇게 push로 할거면 i루프 안의 j 루프를 ~i까지로 해야 한다는 거
-그렇게 안하면 완성되지 않은 dp 칸을 사용해 다음 dp값을 계산하는 불상사가 발생함
-
-pull로 할거면 dp[i]: = dp[j] + dp[i-j] 로 하면 되는데, 이건 불상사 생각 안해도 되는 듯
-대신 이때 종료조건 위치랑 어차피 1,3 / 2,2 / 3,1 처럼 바뀐 상황을 자동으로 계산하니
-아래 push처럼 n, m 자리바꿈 계산은 안해도 됨
-"""
+def myp(arr):
+    for row in arr:
+        for val in row:
+            print(val, end=' ')
+        print()
+    print()
 
 def solution(N, number):
-    answer = 10
+    answer = -1
     
+    """
+    arr[i][j]: i~j번째 까지로 만든 계산 결과 set
+    k 루프
+    k개씩만 하면서 사칙연산
+    
+    dp[i]: N을 i번 써서 만들 수 있는 숫자 set
+    1번써서 만든 거 계산 (0/1)
+    2번 써서 만든 거 계산 (0/2 1/1)
+    3번 써서 만든 거 계산 (0/3 1/2)
+    4번 써서 만든 거 계산 (0/4 1/3 2/2)
+    """
     dp = [set() for _ in range(9)]
-    # dp[0].add(0)
-    
-    for i in range(1, 5):
-        dp[i].add(int(str(N)*i))
     
     for i in range(1, 9):
-        if number in dp[i]:
-            return i
+        dp[i].add(int(str(N)*i))
         
-        for j in range(1, i+1):
-            if i + j > 8:
-                continue
-                
-            for n in dp[j]:
-                for m in dp[i]:
-                    if n - m > 0:
-                        dp[i+j].add(n - m)
-                    elif m - n > 0:
-                        dp[i+j].add(m - n)
-                    dp[i+j].add(n + m)
-                    dp[i+j].add(n * m)
-                    if m > 0:
-                        dp[i+j].add(n / m)
-                    if n > 0:
-                        dp[i+j].add(m / n)
+        # 지금 i값에서 나오는 조합들 탐색
+        for k in range(i):
+            j = i - k
+            for a in dp[j]:
+                for b in dp[k]:
+                    dp[i].add(a + b)
+                    dp[i].add(abs(a - b))
+                    dp[i].add(a * b)
+                    if b != 0:
+                        dp[i].add(a // b)
             
-    return -1
+    for i in range(1, 9):
+        if number in dp[i]:
+            answer = i
+            break
+    
+    return answer
