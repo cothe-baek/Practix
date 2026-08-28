53e83cc65f8503bad863df422eeb0dc324971767
cothe-baek <gyuul173@gmail.com>
2026-08-28 16:32:57 +0900

    [level 3] Title: 등굣길, Time: 5.52 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/Python/3. 등굣길/등굣길.py
+++ 프로그래머스/Python/3. 등굣길/등굣길.py
@@ -1,22 +1,35 @@
+def myp(arr):
+    for row in arr:
+        for val in row:
+            print(val, end=' ')
+        print()
+    print()
+
+
 def solution(M, N, puddles):
-    answer = 0
+    """
+    dp[i][j] = i, j로 가는 최단 경로의 개수
+    """
+    arr = [[0] * M for _ in range(N)]
+    for i, j in puddles:
+        arr[j-1][i-1] = 1
     
-    arr = [[0]*M for _ in range(N)]
     dp = [[0]*M for _ in range(N)]
     dp[0][0] = 1
     
-    for j, i in puddles:
-        arr[i-1][j-1] = 1
-    
     for i in range(N):
         for j in range(M):
+            if arr[i][j] == 1:
+                continue
+                
             # 오른쪽
-            if j < M-1 and not arr[i][j+1]:
-                dp[i][j+1] = dp[i][j+1] + dp[i][j]
+            if j < M-1 and arr[i][j+1] == 0:
+                dp[i][j+1] += dp[i][j]
             
-            # 아래쪽
-            if i < N-1 and not arr[i+1][j]:
-                dp[i+1][j] = dp[i+1][j] + dp[i][j]
+            # 아래
+            if i < N-1 and arr[i+1][j] == 0:
+                dp[i+1][j] += dp[i][j]
     
+    myp(dp)
     
-    return dp[N-1][M-1] % (10**9 + 7)
+    return dp[N-1][M-1] % 1000000007
