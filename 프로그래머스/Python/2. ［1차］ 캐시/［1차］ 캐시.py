eb9376099791462e02d1aba493aac0675770f968
cothe-baek <gyuul173@gmail.com>
2026-09-26 22:06:02 +0900

    [level 2] Title: [1차] 캐시, Time: 22.93 ms, Memory: 17.6 MB -BaekjoonHub

--- 프로그래머스/Python/2. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/Python/2. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,26 +1,20 @@
+from collections import OrderedDict
 def solution(cacheSize, cities):
-    from collections import deque
-    cache = {}
-    q = deque()
+    cache = OrderedDict()
     answer = 0
     
     for i, city in enumerate(cities):
         city = city.lower()
+        
         if city in cache:
             answer += 1
+            cache.move_to_end(city)
         else:
             answer += 5
         
-        q.append((i, city))
         cache[city] = i
-        
-        if len(cache) > cacheSize:
-            while True:
-                pop_i, pop_city = q.popleft()
-                # 유요한 걸 pop하고 처리하기
-                if pop_city in cache and cache[pop_city] == pop_i:
-                    del cache[pop_city]
-                    break
-        
+        # print(cache)
+        while len(cache) > cacheSize:
+            cache.popitem(last=False)
             
     return answer
